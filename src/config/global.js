97b16c55d7c811aa110dc2b388d793c942ad9f16
Andrea Botello <abotellod@gmail.com>
2026-03-11 10:55:36 -0500

    Ajustes CFA [2.0]

--- src/config/global.js
+++ src/config/global.js
@@ -177,7 +177,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_12310041_DU.pdf',
       },
       {
         icono: 'fas fa-download',
